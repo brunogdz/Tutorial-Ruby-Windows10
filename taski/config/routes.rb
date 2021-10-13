a93816973f963021f6102bb8a8be2cdf6b9a25b6
Bruno Gomes <brunog.vix@gmail.com>
2021-10-12 22:11:54 -0300

    error page working

--- taski/config/routes.rb
+++ taski/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'contact', to: "pages#contact"
   get 'about', to: "pages#about"
+  get 'error', to: "pages#error"
 
   get "blog",to: redirect("https://github.com/brunogomes98")
 
   resources :projects
 
   root 'pages#home'
+
+  get "*path", to: redirect("/error")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

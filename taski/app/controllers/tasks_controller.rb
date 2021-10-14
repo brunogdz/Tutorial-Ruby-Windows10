4d532b75342aea5ea53a3e92729bfb8ce100d606
Bruno Gomes <brunog.vix@gmail.com>
2021-10-13 23:25:48 -0300

    criando um controller para tasks

---
+++ taski/app/controllers/tasks_controller.rb
@@ -0,0 +1,12 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
